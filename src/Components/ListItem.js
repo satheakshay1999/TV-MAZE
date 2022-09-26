39f8006375948db5119ffe1d7081f958df4e75e8
satheakshay1999 <88333068+satheakshay1999@users.noreply.github.com>
2022-09-26 14:15:51 +0530

    build

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const ListItem = ({ image, name, rating, id }) => {
   return (
     <div className="listitem">
-    <Link to={`/singleshow/${id}`} >
+    {/* <Link to={`/singleshow/${id}`} > */}
       <img src={image} alt={name} />
       <div className="listitem__info">
         <h4 className="info__name">{name}</h4>
         <h4 className="info__rating">{rating}</h4>
       </div>
-    </Link>
+    {/* </Link> */}
     </div>
   );
 };
